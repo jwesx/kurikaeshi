9a893750dffe2d3c8093511f6a03c36a651c76be
jwsxs <106886827+kcabdof@users.noreply.github.com>
2024-07-04 07:22:53 +0000

    now it works

--- commands/random.py
+++ commands/random.py
@@ -5,10 +5,23 @@ async def randomInt(message):
     n1 = int(message.content.split()[1])
     n2 = int(message.content.split()[2])
 
-    choosen = random.randint(n1, n2)
-    await message.channel.send(
-        random.choice[f"Acho que {choosen} é um número bom...",
-                      f"Talvez {choosen}?", f"E se {choosen} fosse o certo?",
-                      f"Definitivamente {choosen}.", f"{choosen}",
-                      f"{choosen}, por favor.", f"Não sei... {choosen}?"]
-    )
+    if n2 != None:
+        choosen = random.randint(n1, n2)
+        await message.channel.send(f"De {n1} até {n2}...")
+        await message.channel.send(
+            random.choice([
+                f"Acho que {choosen} é um número bom...", f"Talvez {choosen}?",
+                f"E se {choosen} fosse o certo?",
+                f"Definitivamente {choosen}.", f"{choosen}",
+                f"{choosen}, por favor.", f"Não sei... {choosen}?"
+            ]))
+    else:
+        choosen = random.randint(1, n1)
+        await message.channel.send(f"De 1 até {n1}...")
+        await message.channel.send(
+            random.choice([
+                f"Acho que {choosen} é um número bom...", f"Talvez {choosen}?",
+                f"E se {choosen} fosse o certo?",
+                f"Definitivamente {choosen}.", f"{choosen}",
+                f"{choosen}, por favor.", f"Não sei... {choosen}?"
+            ]))
