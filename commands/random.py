df22112bc3f654584b492ae85b86b111aee9f0e7
jwsxs <106886827+kcabdof@users.noreply.github.com>
2024-07-04 09:21:12 +0000

    bunch more stuff on random.py

--- commands/random.py
+++ commands/random.py
@@ -2,10 +2,19 @@ import random
 
 
 async def randomInt(message):
-    n1 = int(message.content.split()[1])
-    n2 = int(message.content.split()[2])
+    words = message.content.split()
+    if len(words) == 1:
+        await message.channel.send(
+            "E os números, fella?\n.!random <número> <número>\nSe colocar apenas um, escolho entre 1 e tal número."
+        )
+    elif len(words) >= 3:
+        n1 = int(message.content.split()[1])
+        n2 = int(message.content.split()[2])
 
-    if n2 != None:
+        if n1 > n2:
+            await message.channel.send(
+                "O primeiro número tem que ser menor que o segundo!")
+
         choosen = random.randint(n1, n2)
         await message.channel.send(f"De {n1} até {n2}...")
         await message.channel.send(
@@ -16,6 +25,10 @@ async def randomInt(message):
                 f"{choosen}, por favor.", f"Não sei... {choosen}?"
             ]))
     else:
+        n1 = int(message.content.split()[1])
+        if n1 == 0:
+            await message.channel.send("De 0 até 0..., eu escolho 0.")
+
         choosen = random.randint(1, n1)
         await message.channel.send(f"De 1 até {n1}...")
         await message.channel.send(
