e0f9197b1280cbd8dd48412e2448b9895ed0656a
Jesus <mapmaking49@gmail.com>
2024-07-04 04:08:50 -0300

    fix stuff

--- commands/random.py
+++ commands/random.py
@@ -2,7 +2,7 @@ import random
 
 async def randomInt(n1, n2):
     choosen = [random.randint(n1, n2)]
-    await message.channel.send(f"{random.choice[
+    await message.channel.send({random.choice[
         f"Acho que {choosen} é um número bom...",
         f"Talvez {choosen}?",
         f"E se {choosen} fosse o certo?",
@@ -10,4 +10,4 @@ async def randomInt(n1, n2):
         f"{choosen}",
         f"{choosen}, por favor.",
         f"Não sei... {choosen}?"
-    ]}")
+    ]})
