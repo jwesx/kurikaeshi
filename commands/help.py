4bb96dfceed117737aa8c6226902507488cd47e9
Jesus <mapmaking49@gmail.com>
2024-07-05 02:16:58 -0300

    more stuff

--- commands/help.py
+++ commands/help.py
@@ -8,8 +8,11 @@ async def helpMessage(message):
       "Eis eles aqui:" +
       
       "**• .!help** → esse mesmo que você acabou de usar!" +
-      "**• .!random** <número> <número> → escolho um número aleatório entre os dois que você escolher (se me falar só um, vou a partir de 1)!" +
+      "**• .!random** {número} <número> → escolho um número aleatório entre os dois que você escolher (se me falar só um, vou a partir de 1)!" +
       "**• .!clear** <quantidade> → limpo as mensagens do canal, com a quantidade que você quiser!" + 
       "**• .!8ball** <pergunta> → faça uma pergunta para mim, e eu te respondo!" +
-      "**• .!dm** <@user> <mensagem> → envio uma mensagem direta para o cara"
+      "**• .!dm** <@user> <mensagem> → envio uma mensagem direta para a pessoa." +
+      "**• .!guessgame** {number} → jogue um jogo de adivinhar o número! Fique atento ao que eu mandar após sua tentativa." +
+
+      "Para melhor compreensão desse comando, os que estiverem entre <> são obrigatórios, e entre {} é opcional."
   )
